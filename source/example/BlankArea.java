9cecb4425ab74e14d163931a1e2b6c371a66e632
U-DAVEPC2\Kevin <Kevin@davepc2.(none)>
2009-07-17 12:03:57 -0400

    brought in working example

---
+++ source/example/BlankArea.java
@@ -0,0 +1,25 @@
+package example;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
+public class BlankArea extends JLabel {
+	  Dimension minSize = new Dimension(100, 100);
+
+	  public BlankArea(Color color) {
+	    setBackground(color);
+	    setOpaque(true);
+	    setBorder(BorderFactory.createLineBorder(Color.black));
+	  }
+
+	  public Dimension getMinimumSize() {
+	    return minSize;
+	  }
+
+	  public Dimension getPreferredSize() {
+	    return minSize;
+	  }
+	}
