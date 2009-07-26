563d158aa4e4bc4610ff8fe3cd46dccb49b9e588
U-DAVEPC2\Kevin <Kevin@davepc2.(none)>
2009-07-26 19:56:50 -0400

    not sure what im doing right now

--- source/pipe/core/museum/SendPipe.java
+++ source/pipe/core/museum/SendPipe.java
@@ -8,38 +8,17 @@ import base.file.Path;
 import base.state.Close;
 
 public class SendPipe extends Close implements Pipe {
+
+	@Override public String title() { return "Send Pipe"; }
+	@Override public String instruction() { return "Choose the folder you want to send:"; }
 	
 	public SendPipe(Program program) {
 		this.program = program;
 		panel = new PipePanel(program, this);
 		info = new PipeInfoFrame(program, this);
-		
-		
-		/*
-		new ExchangeDialog(program, pipe);
-		
-		if (pipe.readyToStart()) {
-			
-			program.core.pipes.add(pipe);
-			program.core.changed();
-			
-		} else {
-			
-			close(pipe);
-		}
-		*/
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 	
 	private final Program program;
-	
 
 	@Override public void close() {
 		if (already()) return;
@@ -47,15 +26,13 @@ public class SendPipe extends Close implements Pipe {
 		close(info);
 	}
 	
-	private final PipePanel panel;
 	@Override public PipePanel panel() { return panel; }
+	private final PipePanel panel;
 	
-	private final PipeInfoFrame info;
 	@Override public PipeInfoFrame info() { return info; }
+	private final PipeInfoFrame info;
 
-	@Override
-	public boolean ready() {
-		// TODO Auto-generated method stub
+	@Override public boolean ready() {
 		return false;
 	}
 
@@ -63,26 +40,19 @@ public class SendPipe extends Close implements Pipe {
 		this.folder = p;
 		return folder;
 	}
+	@Override public Path folder() { return folder; }
 	private Path folder;
-	@Override public Path folder() {
-		return folder;
-	}
 
 	@Override public Outline away(Outline away) {
 		this.away = away;
 		return away;
 	}
+	@Override public Outline away() { return away; }
 	private Outline away;
-	@Override public Outline away() {
-		return away;
-	}
 
 	@Override public Outline home() {
 		return new Outline();
 	}
-
-	@Override public String title() { return "Send Pipe"; }
-	@Override public String instruction() { return "Choose the folder you want to send:"; }
 
 
 	@Override public void go() {
