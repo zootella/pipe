616b003f3b9ac2c7d98736e39cddbddbb084de11
U-DAVEPC2\Kevin <Kevin@davepc2.(none)>
2009-08-07 11:15:29 -0400

    before bouncy castle

--- source/pipe/main/AES.java
+++ source/pipe/main/AES.java
@@ -1,65 +1,39 @@
 package pipe.main;
 
+import java.security.InvalidParameterException;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import base.data.Data;
+import base.exception.PlatformException;
+
 public class AES {
 
-	/**
-	 * Turns array of bytes into string
-	 *
-	 * @param buf	Array of bytes to convert to hex string
-	 * @return	Generated hex string
-	 */
-	public static String asHex (byte buf[]) {
-		StringBuffer strbuf = new StringBuffer(buf.length * 2);
-		int i;
+	public static void go() throws Exception {
 
-		for (i = 0; i < buf.length; i++) {
-			if (((int) buf[i] & 0xff) < 0x10)
-				strbuf.append("0");
+		KeyGenerator generate = KeyGenerator.getInstance("AES");//this is really slow, you actually have to do it in a task, have a global static
+		try {
+			generate.init(192); // Choices are 128, 192 and 256
+		} catch (InvalidParameterException e) { throw new PlatformException(e); }
 
-			strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
-		}
+		SecretKey key = generate.generateKey();
+		byte[] raw = key.getEncoded();
+		System.out.println("raw " + new Data(raw).base16());
 
-		return strbuf.toString();
-	}
-
-	public static void main(String[] args) throws Exception {
-
-		String message="This is just an example";
-
-		// Get the KeyGenerator
-
-		KeyGenerator kgen = KeyGenerator.getInstance("AES");
-		kgen.init(128); // 192 and 256 bits may not be available
-
-
-		// Generate the secret key specs.
-		SecretKey skey = kgen.generateKey();
-		byte[] raw = skey.getEncoded();
-
-		SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-
-
-		// Instantiate the cipher
+		SecretKeySpec key2 = new SecretKeySpec(raw, "AES");
 
 		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key2);
 
-		cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+		byte[] encrypted = cipher.doFinal("This is just an example".getBytes());
+		System.out.println("encrypted string: " + new Data(encrypted).base16());
 
-		byte[] encrypted =
-			cipher.doFinal((args.length == 0 ?
-					"This is just an example" : args[0]).getBytes());
-		System.out.println("encrypted string: " + asHex(encrypted));
-
-		cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-		byte[] original =
-			cipher.doFinal(encrypted);
+		cipher.init(Cipher.DECRYPT_MODE, key2);
+		byte[] original = cipher.doFinal(encrypted);
 		String originalString = new String(original);
-		System.out.println("Original string: " +
-				originalString + " " + asHex(original));
+		System.out.println("Original string: " + originalString + " " + new Data(original).base16());
 	}
 }
--- source/pipe/main/Snippet.java
+++ source/pipe/main/Snippet.java
@@ -1,11 +1,15 @@
 package pipe.main;
 
+import base.process.Mistake;
+
 public class Snippet {
 
 	public static void snippet(Program program) {
 
 
-		
+		try {
+			AES.go();
+		} catch (Exception e) { Mistake.stop(e); }
 
 	}
 }
