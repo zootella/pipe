adfb7e8fb29419c49c66b00ff5097bdb5431857a
U-DAVEPC2\Kevin <Kevin@davepc2.(none)>
2009-08-07 11:36:27 -0400

    before trying to fix it

--- source/pipe/main/AES.java
+++ source/pipe/main/AES.java
@@ -1,11 +1,15 @@
 package pipe.main;
 
 import java.security.InvalidParameterException;
+import java.security.Security;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import base.data.Data;
 import base.exception.PlatformException;
@@ -13,19 +17,23 @@ import base.exception.PlatformException;
 public class AES {
 
 	public static void go() throws Exception {
+		
+		
+		Security.addProvider(new BouncyCastleProvider());
 
-		KeyGenerator generate = KeyGenerator.getInstance("AES");//this is really slow, you actually have to do it in a task, have a global static
+		KeyGenerator generate = KeyGenerator.getInstance("AES", "BC");//this is really slow, you actually have to do it in a task, have a global static
 		try {
-			generate.init(192); // Choices are 128, 192 and 256
+			generate.init(256); // Choices are 128, 192 and 256
 		} catch (InvalidParameterException e) { throw new PlatformException(e); }
 
 		SecretKey key = generate.generateKey();
 		byte[] raw = key.getEncoded();
 		System.out.println("raw " + new Data(raw).base16());
-
+		
+		
 		SecretKeySpec key2 = new SecretKeySpec(raw, "AES");
 
-		Cipher cipher = Cipher.getInstance("AES");
+		Cipher cipher = Cipher.getInstance("AES", "BC");
 		cipher.init(Cipher.ENCRYPT_MODE, key2);
 
 		byte[] encrypted = cipher.doFinal("This is just an example".getBytes());
